e7edc55376f01e196aad8d241f14f992a597d163
gwe9001 <gwe9001@gmail.com>
2022-06-10 21:01:48 +0800

    商品功能　- 實作刪除商品

--- src/main/java/net/chunchieh/springbootmall/dao/impl/ProductDaoImpl.java
+++ src/main/java/net/chunchieh/springbootmall/dao/impl/ProductDaoImpl.java
@@ -71,10 +71,10 @@ public class ProductDaoImpl implements ProductDao {
     public void updateProduct(Integer productId, ProductRequest productRequest) {
         String sql = "UPDATE product SET product_name = :productName, category = :category, image_url = :imageUrl, " +
                 "price = :price, stock = :stock, description = :description, last_modified_date = :lastModifiedDate" +
-                " WHERE product_id = :product_id";
+                " WHERE product_id = :productId";
         Map<String,Object> map = new HashMap<>();
 
-        map.put("product_id",productId);
+        map.put("productId",productId);
 
         map.put("productName",productRequest.getProductName());
         map.put("category",productRequest.getCategory().toString());
@@ -87,5 +87,15 @@ public class ProductDaoImpl implements ProductDao {
 
         namedParameterJdbcTemplate.update(sql,map);
 
+    }
+
+    @Override
+    public void deleteProductById(Integer productId) {
+        String sql = "DELETE FROM product WHERE product_id = :productId";
+
+        Map<String ,Object> map = new HashMap<>();
+        map.put("productId",productId);
+
+        namedParameterJdbcTemplate.update(sql,map);
     }
 }
