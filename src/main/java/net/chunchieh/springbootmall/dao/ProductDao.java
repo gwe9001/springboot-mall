10c976bd7c459802b45589c578ee2813733e61e5
gwe9001 <gwe9001@gmail.com>
2022-06-11 15:54:26 +0800

    商品功能　- 實作查詢商品列表(基礎實作)

--- src/main/java/net/chunchieh/springbootmall/dao/ProductDao.java
+++ src/main/java/net/chunchieh/springbootmall/dao/ProductDao.java
@@ -3,8 +3,11 @@ package net.chunchieh.springbootmall.dao;
 import net.chunchieh.springbootmall.dto.ProductRequest;
 import net.chunchieh.springbootmall.model.Product;
 
+import java.util.List;
+
 public interface ProductDao {
 
+    List<Product> getProducts();
     Product getProductById(Integer productId);
 
     Integer createProduct(ProductRequest productRequest);
--- src/main/java/net/chunchieh/springbootmall/service/ProductService.java
+++ src/main/java/net/chunchieh/springbootmall/service/ProductService.java
@@ -3,8 +3,11 @@ package net.chunchieh.springbootmall.service;
 import net.chunchieh.springbootmall.dto.ProductRequest;
 import net.chunchieh.springbootmall.model.Product;
 
+import java.util.List;
+
 public interface ProductService {
 
+    List<Product> getProducts();
     Product getProductById(Integer productId);
 
     Integer createProduct(ProductRequest productRequest);
