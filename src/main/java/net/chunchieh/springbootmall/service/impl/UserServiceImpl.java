e23c944d64a4b7a89631862652b32340d5cbd7e1
gwe9001 <gwe9001@gmail.com>
2022-06-22 19:48:50 +0800

    帳號功能　- 檢查註冊的 email、隱藏返回的密碼

--- src/main/java/net/chunchieh/springbootmall/service/impl/UserServiceImpl.java
+++ src/main/java/net/chunchieh/springbootmall/service/impl/UserServiceImpl.java
@@ -4,11 +4,17 @@ import net.chunchieh.springbootmall.dao.UserDao;
 import net.chunchieh.springbootmall.dto.UserRegisterRequest;
 import net.chunchieh.springbootmall.model.User;
 import net.chunchieh.springbootmall.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.web.server.ResponseStatusException;
 
 @Component
 public class UserServiceImpl implements UserService {
+
+    private  final static Logger log = LoggerFactory.getLogger(UserServiceImpl.class);
 
     @Autowired
     private UserDao userDao;
@@ -22,6 +28,17 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public Integer register(UserRegisterRequest userRegisterRequest) {
+        // 檢查註冊的 email
+        User user = userDao.getUserByEmail(userRegisterRequest.getEmail());
+
+
+        if(user != null){
+            //在Log的warn級別紀錄資訊
+            log.warn("該 email {} 已經被註冊了",userRegisterRequest.getEmail());
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+
+        //創建帳號
         return userDao.createUser(userRegisterRequest);
     }
 
